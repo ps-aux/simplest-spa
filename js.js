975e61e4a48500ca1cd71ed754e23e59b7c3f821
psaux <seriouz.accz@gmail.com>
2019-10-15 12:44:42 +0200

    Add basic html rendering

--- js.js
+++ js.js
@@ -1,5 +1,18 @@
-fetch('/data/team/eam.json')
-  .then(r => r.text())
-  .then(team => {
-    console.log('team', team)
+fetch('/data/team/alice.json')
+  .then(r => r.json())
+  .then(p => {
+
+    const cont = document.createElement('div')
+
+    const id = document.createElement('span')
+    id.innerText = 'id: ' + p.id
+    cont.append(id)
+
+    cont.append(document.createElement('div'))
+
+    const name = document.createElement('span')
+    name.innerText = 'name: ' + p.name
+    cont.append(name)
+
+    document.body.append(cont)
   })
